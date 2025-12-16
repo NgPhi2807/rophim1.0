05312bc013c0bb163ff1c666d5b644967a0b1cf1
Nguyễn Hùng Phi <157231268+NgPhi2807@users.noreply.github.com>
2025-12-16 11:47:42 +0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless"; // dùng bản serverless
+import node from "@astrojs/node";
 import compress from "astro-compress";
 
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: node({ mode: "standalone" }),
 
   integrations: [
     tailwind(),
@@ -19,4 +19,15 @@ export default defineConfig({
       img: true,
     }),
   ],
+
+  vite: {
+    build: {
+      sourcemap: false,
+    },
+  },
+
+  server: {
+    port: 4555,
+    host: true,
+  },
 });
